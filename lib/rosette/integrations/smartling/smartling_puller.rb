e53a78c214c1ebf034496854b75401ca8b08a70a
Matt Low <mlow@lumoslabs.com>
2014-10-14 12:11:04 -0700

    Add translations to DB from Smartling pull

--- lib/rosette/integrations/smartling/smartling_puller.rb
+++ lib/rosette/integrations/smartling/smartling_puller.rb
@@ -5,21 +5,43 @@ module Rosette
     module Smartling
       class SmartlingPuller < SmartlingOperation
 
-        attr_reader :datastore
+        attr_reader :configuration
 
-        def initialize(datastore, api_options = {})
+        def initialize(configuration, api_options = {})
           super(api_options)
-          @datastore = datastore
+          @configuration = configuration
         end
 
-        def pull(locale)
+        def pull(locale, extractor_id, encoding = nil)
           file_list_response = api.list(locale: locale)
           file_list = SmartlingFileList.from_api_response(file_list_response)
 
           file_list.each do |file|
-            datastore.add_or_update_commit_log_locale(
+            configuration.datastore.add_or_update_commit_log_locale(
               file.commit_id, locale, file.translated_count
             )
+
+            repo_config = configuration.get_repo(file.repo_name)
+            encodings = repo_config.extractor_configs.map(&:encoding).uniq
+
+            if encodings.size > 1 && !encoding
+              raise 'More than one encoding found. Please specify encoding.'
+            else
+              encoding = encoding || encodings.first
+              file_contents = api.download(file.file_uri, locale: locale).force_encoding(encoding)
+
+              extractor = Rosette::Core::ExtractorId.resolve(extractor_id).new
+
+              extractor.extract_each_from(file_contents) do |phrase_object|
+                configuration.datastore.add_or_update_translation(repo_config.name, {
+                    phrase_object.index_key => phrase_object.index_value,
+                    commit_id: file.commit_id,
+                    translation: phrase_object.key,
+                    locale: locale
+                  }
+                )
+              end
+            end
           end
         end
 
