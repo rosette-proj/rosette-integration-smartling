013bc76bd5569deadb75329bf7683da26393dcdb
Matt Low <mlow@lumoslabs.com>
2014-10-16 14:16:46 -0700

    Update commit log w/ translated status
    Before file deletion

--- lib/rosette/integrations/smartling/smartling_completer.rb
+++ lib/rosette/integrations/smartling/smartling_completer.rb
@@ -13,11 +13,14 @@ module Rosette
         def complete(locales)
           file_hash ||= Hash.new{ |h, key| h[key] = [] }
 
+          file_uris_to_files = {}
           locales.each do |locale|
             file_list_response = api.list(locale: locale)
             file_list = SmartlingFileList.from_api_response(file_list_response)
 
             file_list.each do |file|
+              file_uris_to_files[file.file_uri] ||= file
+
               if file.phrase_count == file.translated_count
                 file_hash[file.file_uri] << locale
               end
@@ -27,6 +30,10 @@ module Rosette
           file_hash.each do |file_uri, completed_locales|
             if completed_locales.sort == locales.sort
               Rosette.logger.info("Deleting file from Smartling: #{file_uri}")
+              file = file_uris_to_files[file_uri]
+              configuration.datastore.add_or_update_commit_log(
+                file.repo_name, file.commit_id, Rosette::DataStores::PhraseStatus::TRANSLATED
+              )
               api.delete(file_uri)
             end
           end
