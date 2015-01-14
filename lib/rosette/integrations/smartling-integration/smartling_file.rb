0c0ec638cec4a574742f6953dab7eacaf4c8e5c9
Cameron Dutro <camertron@gmail.com>
2015-01-14 15:18:51 -0800

    SmartlingPusher should upload every file in the snapshot
    This fixes issues where translations have been modified in Smartling
    but nobody’s uploaded the file they originated from for a long time.

--- lib/rosette/integrations/smartling-integration/smartling_file.rb
+++ lib/rosette/integrations/smartling-integration/smartling_file.rb
@@ -5,11 +5,13 @@ module Rosette
     class SmartlingIntegration < Integration
       class SmartlingFile
 
-        attr_reader :repo_name, :commit_id, :phrase_count, :translated_count, :file_uri
+        attr_reader :repo_name, :commit_id, :file
+        attr_reader :phrase_count, :translated_count, :file_uri
 
-        def initialize(repo_name, commit_id, phrase_count, translated_count, file_uri)
+        def initialize(repo_name, commit_id, file, phrase_count, translated_count, file_uri)
           @repo_name = repo_name
           @commit_id = commit_id
+          @file = file
           @phrase_count = phrase_count
           @translated_count = translated_count
           @file_uri = file_uri
@@ -17,12 +19,15 @@ module Rosette
 
         def self.from_api_response(response)
           file_uri = response['fileUri']
-          repo_name = File.dirname(File.dirname(file_uri))
-          commit_id = File.basename(file_uri).chomp(File.extname(file_uri))
+          repo_name, author, commit_id, file = file_uri.split('/')
+          file = file.chomp(File.extname(file))
           phrase_count = response['stringCount']
           translated_count = response['completedStringCount']
 
-          new(repo_name, commit_id, phrase_count, translated_count, file_uri)
+          new(
+            repo_name, commit_id, file, phrase_count,
+            translated_count, file_uri
+          )
         end
 
         def self.list_from_api_response(response)
--- lib/rosette/integrations/smartling-integration/smartling_pusher.rb
+++ lib/rosette/integrations/smartling-integration/smartling_pusher.rb
@@ -21,18 +21,22 @@ module Rosette
         end
 
         def push(commit_id, serializer_id)
+          phrases_by_file = phrases_by_file_for(commit_id)
           serializer_const = Rosette::Core::SerializerId.resolve(serializer_id)
-          destination_filenames(commit_id, serializer_const).map do |destination|
-            file_for_upload(commit_id, serializer_const) do |tmp_file|
+
+          phrases_by_file.each_pair do |file, phrases_for_file|
+            dest_filename = destination_filename_for(file, phrases_for_file, serializer_const)
+
+            file_for_upload(phrases_for_file, serializer_const) do |tmp_file|
               response = smartling_api.upload(
                 tmp_file.path,
-                destination,
+                dest_filename,
                 file_type_for(serializer_id), {
                   approved: smartling_api.preapprove_translations?
                 }
               )
-              phrase_count = response['stringCount']
 
+              phrase_count = response['stringCount']
               rosette_config.datastore.add_or_update_commit_log(
                 repo_name, commit_id, nil, Rosette::DataStores::PhraseStatus::PENDING, phrase_count
               )
@@ -45,6 +49,14 @@ module Rosette
         end
 
         private
+
+        def phrases_by_file_for(commit_id)
+          Rosette::Core::Commands::SnapshotCommand.new(rosette_config)
+            .set_repo_name(repo_name)
+            .set_commit_id(commit_id)
+            .execute
+            .group_by(&:file)
+        end
 
         # For serializer ids like yaml/rails and json/key-value, the
         # file type can be inferred from the first half (i.e. 'yaml'
@@ -62,24 +74,25 @@ module Rosette
           end
         end
 
-        def destination_filenames(commit_id, serializer_const)
-          repo = rosette_config.get_repo(repo_name).repo
-          rev_commit = repo.get_rev_commit(commit_id)
+        def destination_filename_for(file, phrases_for_file, serializer_const)
+          unless phrases_for_file.empty?
+            repo = rosette_config.get_repo(repo_name).repo
+            commit_id = phrases_for_file.first.commit_id
+            rev_commit = repo.get_rev_commit(commit_id)
 
-          Array(
             File.join(
-              repo_name, get_identity_string(rev_commit),
-              "#{commit_id}#{serializer_const.default_extension}"
+              repo_name,
+              get_identity_string(rev_commit),
+              commit_id,
+              "#{sanitize_path(file)}#{serializer_const.default_extension}"
             )
-          )
+          end
         end
 
         # @TODO: this will need to change if we inline phrases because
         # we might not have meta_keys anymore (this method assumes we do)
-        def file_for_upload(commit_id, serializer_const)
-          phrases = rosette_config.datastore.phrases_by_commit(repo_name, commit_id)
-
-          if phrases.any?
+        def file_for_upload(phrases, serializer_const)
+          unless phrases.empty?
             Tempfile.open(['rosette', serializer_const.default_extension]) do |file|
               serializer = serializer_const.new(file, rosette_config.get_repo(repo_name).source_locale)
               serializer.write_raw(integration_config.directives + "\n")
@@ -113,6 +126,10 @@ module Rosette
             index = email.index('@') || 0
             email[0..index - 1].gsub(/[^\w]/, '')
           end
+        end
+
+        def sanitize_path(path)
+          path.gsub('/', '$')
         end
 
       end
