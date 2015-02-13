6a5d20bd83c3d6d1ffbad5794ed42eb06fecbbe4
Cameron Dutro <camertron@gmail.com>
2015-02-13 11:59:58 -0800

    Add exponential back off to retrier

--- lib/rosette/integrations/smartling-integration/retrier.rb
+++ lib/rosette/integrations/smartling-integration/retrier.rb
@@ -6,6 +6,7 @@ module Rosette
 
       class Retrier
         DEFAULT_MAX_RETRIES = 5
+        DEFAULT_BASE_SLEEP_SECONDS = 1
 
         def self.retry(options = {}, &block)
           new(options.fetch(:times, DEFAULT_MAX_RETRIES), block)
@@ -19,8 +20,8 @@ module Rosette
           @proc = proc
         end
 
-        def on_error(error_class, &block)
-          @errors[error_class] = block
+        def on_error(error_class, options = {}, &block)
+          @errors[error_class] = { proc: block, options: options }
           self
         end
 
@@ -29,10 +30,56 @@ module Rosette
           begin
             proc.call
           rescue *errors.keys => e
+            handler_classes = handler_classes_for(e.class)
+
+            handler_class = handler_classes.find do |handler_class|
+              options = errors.fetch(handler_class, {}).fetch(:options, {})
+              should_have_rescued?(e, options)
+            end
+
+            handler = errors[handler_class]
+            raise e unless handler
+            options = handler.fetch(:options, {})
+
             retries += 1
-            errors[e.class].call(e, retries) if errors[e.class]
+            handler[:proc].call(e, retries) if handler[:proc]
+            sleep calc_sleep_time(retries, options)
             retry unless retries >= max_retries
             raise e
+          end
+        end
+
+        private
+
+        def handler_classes_for(klass)
+          errors.each_with_object([]) do |(error_class, _), ret|
+            if ancestor_class = ancestor_handler_class_for(error_class)
+              ret << ancestor_class
+            end
+          end
+        end
+
+        def ancestor_handler_class_for(klass)
+          klass.ancestors.find do |ancestor|
+            errors.include?(ancestor)
+          end
+        end
+
+        def should_have_rescued?(error, options)
+          regex = options.fetch(:message, //)
+          !!(error.message =~ regex)
+        end
+
+        def calc_sleep_time(retries, options)
+          if options[:backoff]
+            base_sleep_seconds = options.fetch(
+              :base_sleep_seconds, DEFAULT_BASE_SLEEP_SECONDS
+            )
+
+            # exponential backoff
+            base_sleep_seconds * (2 ** (retries - 1))
+          else
+            0
           end
         end
       end
--- spec/retrier_spec.rb
+++ spec/retrier_spec.rb
@@ -22,8 +22,9 @@ describe SmartlingIntegration::SmartlingApi do
   let(:retrier) { SmartlingIntegration::Retrier }
   let(:error) { RetryError }
   let(:tracker) { RetryTracker.new }
+  let(:error_message) { "I'm a little teapot" }
   let(:track_proc) do
-    Proc.new { tracker.increment && raise(RetryError) }
+    Proc.new { tracker.increment && raise(RetryError, error_message) }
   end
 
   it 'catches errors and retries the specified number of times' do
@@ -56,6 +57,16 @@ describe SmartlingIntegration::SmartlingApi do
     expect(tracker.count).to eq(1)
   end
 
+  it "re-raises errors if the message doesn't match" do
+    expect do
+      retrier.retry(times: 3, &track_proc)
+        .on_error(StandardError, message: /FOOBAR/)
+        .execute
+    end.to raise_error(RetryError)
+
+    expect(tracker.count).to eq(1)
+  end
+
   it 'calls the block associated with the error' do
     sentinel = nil
 
@@ -64,5 +75,40 @@ describe SmartlingIntegration::SmartlingApi do
       .execute
 
     expect(sentinel).to eq(:hit)
+  end
+
+  it 'moves down the chain of errors' do
+    sentinel = nil
+
+    expect do
+      retrier.retry(times: 3, &track_proc)
+        .on_error(RetryError, message: /FOOBAR/)
+        .on_error(StandardError) { sentinel = :hit }
+        .execute
+    end.to raise_error(RetryError)
+
+    expect(sentinel).to eq(:hit)
+  end
+
+  it 'sleeps exponentially if asked to back off' do
+    trier = retrier.retry(times: 3, &track_proc)
+      .on_error(RetryError, backoff: true)
+
+    3.times do |i|
+      expect(trier).to receive(:sleep).with(2 ** i)
+    end
+
+    expect { trier.execute }.to raise_error(RetryError)
+  end
+
+  it 'sleeps an exponentially increasing number of seconds with the given base' do
+    trier = retrier.retry(times: 3, &track_proc)
+      .on_error(RetryError, backoff: true, base_sleep_seconds: 2)
+
+    3.times do |i|
+      expect(trier).to receive(:sleep).with(2 * (2 ** i))
+    end
+
+    expect { trier.execute }.to raise_error(RetryError)
   end
 end
