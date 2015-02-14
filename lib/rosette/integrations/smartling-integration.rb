0c6d69bb997777a45d57ce989aeaedb97050a090
Cameron Dutro <camertron@gmail.com>
2015-02-14 00:53:38 -0800

    Remove after commit hook :(

--- lib/rosette/integrations/smartling-integration.rb
+++ lib/rosette/integrations/smartling-integration.rb
@@ -24,9 +24,7 @@ module Rosette
       end
 
       def integrate(obj)
-        if integrates_with?(obj)
-          integrate_with_repo_config(obj)
-        else
+        unless integrates_with?(obj)
           raise Errors::ImpossibleIntegrationError,
             "Cannot integrate #{self.class.name} with #{obj}"
         end
@@ -39,26 +37,6 @@ module Rosette
       def smartling_api
         @smartling_api ||=
           SmartlingApi.new(configuration.api_options)
-      end
-
-      private
-
-      def integrate_with_repo_config(obj)
-        obj.after(:commit) do |rosette_config, repo_config, commit_id|
-          smartling_pusher(rosette_config, repo_config.name).push(
-            commit_id, configuration.serializer_id
-          )
-        end
-      end
-
-      def smartling_pusher(rosette_config, repo_name)
-        repo_config = rosette_config.get_repo(repo_name)
-        smartling_pushers[repo_name] ||= SmartlingPusher.new(rosette_config)
-          .set_repo_config(repo_config)
-      end
-
-      def smartling_pushers
-        @smartling_pushers ||= {}
       end
     end
 
