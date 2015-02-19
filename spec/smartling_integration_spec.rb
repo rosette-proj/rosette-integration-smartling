c169f0bd44c52e6d5fa60b072cb8c80fef21df89
Cameron Dutro <camertron@gmail.com>
2015-02-19 12:00:46 -0800

    Remove hook check in integration spec

--- spec/smartling_integration_spec.rb
+++ spec/smartling_integration_spec.rb
@@ -29,15 +29,4 @@ describe SmartlingIntegration do
       expect(integration.integrates_with?('foo')).to be(false)
     end
   end
-
-  describe '#integrate' do
-    it 'adds an after commit hook to the repo config' do
-      repo_config = Rosette::Core::RepoConfig.new(repo_name)
-      integration.integrate(repo_config)
-      repo_config.hooks[:after].tap do |after_hooks|
-        expect(after_hooks).to include(:commit)
-        expect(after_hooks[:commit].size).to eq(1)
-      end
-    end
-  end
 end
