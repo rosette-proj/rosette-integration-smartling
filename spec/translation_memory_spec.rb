9fabb4d2b1e218d6571c022b0b4b1fc9ee0dc1b0
abMatGit <amatuszewski@lumoslabs.com>
2015-08-13 15:54:58 -0700

    add complex part of tmx file - causes failures

--- spec/translation_memory_spec.rb
+++ spec/translation_memory_spec.rb
@@ -295,13 +295,20 @@ describe TranslationMemory do
           key: "To ensure uninterrupted access, your subscription will auto-renew every %{rate} "\
                "at a rate of %{renewal_rate} using the payment method provided. "\
                "You may turn off auto-renewal at any time through your Account Settings. "\
+               "By clicking '%{button_phrase}' you allow us to charge the payment method provided, "\
+               "and you agree to our <a href='%{tos_link}' class='tos-link' target='_blank'>"\
+               "Terms of Service</a> and <a href='%{payment_policy_link}' class='payment-policy-link' target='_blank'>Payment Policy</a>."
         )
 
         trans = memory.translation_for(locale, phrase)
         expect(trans).to eq(
           "Um ununterbrochenen Zugriff zu gewährleisten, wird Ihr Abonnement mithilfe der "\
           "hinterlegten Zahlungsmethode automatisch %{renewal_rate} zu einem Preis von %{rate} verlängert. "\
-          "Sie können die automatische Verlängerung jederzeit über Ihre Account-Einstellungen deaktivieren."\
+          "Sie können die automatische Verlängerung jederzeit über Ihre Account-Einstellungen deaktivieren. "\
+          "<a href='%{tos_link}' class='tos-link' target='_blank'></a><a href='%{payment_policy_link}' "\
+          "class='payment-policy-link' target='_blank'></a>Durch einen Klick %{button_phrase}erklären "\
+          "Sie sich damit einverstanden, dass wir den Mitgliedsbeitrag mit der von Ihnen hinterlegten Zahlungsmethode abbuchen. "\
+          "Des Weiteren stimmen Sie unseren Datenschutzrichtlinien und Nutzungsbedingungen zu."
         )
       end
     end
